f127a1c94045356701c1c4c14e2a1409a5e1e8a9
RobbeNooyens <robbenooyens@hotmail.com>
2021-03-10 22:44:59 +0100

    Started testing

---
+++ VaccimulatorTests.cpp
@@ -0,0 +1,9 @@
+//
+// Created by robbe on 10/03/2021.
+//
+#include <gtest/gtest.h>
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
---
+++ tests/domain/VaccinationCenterTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by robbe on 10/03/2021.
+//
+
+#include <gtest/gtest.h>
+
+class VaccinationCenterTests: public ::testing::Test {
+protected:
+    // You should make the members protected s.t. they can be
+    // accessed from sub-classes.
+
+};
